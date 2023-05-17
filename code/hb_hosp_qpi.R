01ee18c4d83e938b30afbf725e29c1c9d429f0db
Angus-Morton <angus.morton@phs.scot>
2023-05-17 18:03:22 +0100

    messing around with snippets

--- code/hb_hosp_qpi.R
+++ code/hb_hosp_qpi.R
@@ -1,5 +1,15 @@
-# hb_hosp_qpi.R
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# SCRIPT_NAME.R
+# Angus Morton
+# DD/MM/YYYY
+# 
+# DESCRIPTION
+# 
+# R version 4.1.2
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#### Step 0 : Housekeeping ----
 
 library(readr)
 library(dplyr)
-library(openxlsx)
+library(tidylog)
