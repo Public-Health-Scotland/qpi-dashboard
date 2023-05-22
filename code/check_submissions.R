f80f051e133eed7707e9213372f9173276cc9439
Angus-Morton <angus.morton@phs.scot>
2023-05-22 12:02:41 +0100

    Created file to check submissions

---
+++ code/check_submissions.R
@@ -0,0 +1,18 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# check_submissions.R
+# Angus Morton
+# 2023-05-22
+# 
+# Checks for the data submissions by the three networks to make sure
+# all data is valid
+# 
+# R version 4.1.2 (2021-11-01)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#### Step 0 : Housekeeping ----
+
+source("code/functions.R")
+source("code/housekeeping.R")
+source("code/packages.R")
+
+###
