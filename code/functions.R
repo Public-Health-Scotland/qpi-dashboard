9fe2042ce8a71c0965ea5c93636d2433698c5b01
Angus-Morton <angus.morton@phs.scot>
2023-06-07 16:18:09 +0100

    Wrote function to read in QPI data from a network submission

--- code/functions.R
+++ code/functions.R
@@ -1,6 +1,63 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # functions.R
+# Angus Morton
+# 2023-06-07
+# 
+# Define functions used in other scripts
+# 
+# R version 4.1.2 (2021-11-01)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# read in and tidy template
+source("code/packages.R")
+
+# read in and tidy qpi info from template
+
+read_qpi_data <- function(fpath) {
+  
+  # Reads in a QPI data submission template from a network
+  
+  template <- loadWorkbook(fpath)
+  
+  years_num <- length(names(template))-1
+  
+  dat <- data.frame()
+  
+  for (year in 1:years_num) {
+    
+    rows <- readWorkbook(wos, sheet = year, startRow = 5, colNames = TRUE,
+                         cols = 2) %>%
+      filter(Network == "WoSCAN") %>% 
+      count(Network) %>% 
+      select(n) %>% 
+      pull()
+    
+    year_dat <- readWorkbook(template, sheet = year, startRow = 5, 
+                             rows = c(5:(rows+5)),
+                             cols = c(2:13)) %>% 
+      mutate(Comments = as.character(Comments),
+             cyear = year)
+    
+    dat <- bind_rows(dat, year_dat)
+    
+  }
+  
+  dat
+  
+}
+
+# read in and tidy qpi info from template
+
+read_background_data <- function(fpath) {
+  
+  # Reads in a QPI data submission template from a network
+  
+  template <- loadWorkbook(fpath)
+
+  
+  data
+  
+}
+
 
 # Check totals match
 
