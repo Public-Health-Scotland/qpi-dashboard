422100e97adc2c4d3b85a321bd0cef659abb5c1c
CodemakerPHS <pauline.ward5@phs.scot>
2024-05-29 10:30:44 +0100

    Checking for missing values in numerator1 column

--- code/hb_hosp_qpi.R
+++ code/hb_hosp_qpi.R
@@ -76,8 +76,9 @@ new_data <- new_data |>
 # Sometimes happens because of a typo in the QPI name. 
 # Checking Numerator1 column as a proxy for the whole row in lookup
 rows_with_missing_values <- new_data |> 
-  filter(str_equal(Numerator1, "") | str_equal(Numerator1, NULL))
-if (nrow(rows_with_missing_values) > 0) {
+  filter(str_equal(numerator1, "") || is.na(numerator1))
+
+if (nrow(rows_with_missing_values) > 0 ) {
   message("ISSUE DETECTED: POSSIBLE UN-MATCHED ROWS.\n")
   message("The Numerator1 column is empty in some rows, indicating possible mis-match between data submissions and lookup, see missing_data.csv.\n")
  write.csv(rows_with_missing_values, file = here(data_folder, "missing_data.csv"))
