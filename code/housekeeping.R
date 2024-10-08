9420900f42dbba30367d2c4b73aed2f257ebadb8
CodemakerPHS <pauline.ward5@phs.scot>
2024-10-08 16:41:48 +0100

    path to input

--- code/housekeeping.R
+++ code/housekeeping.R
@@ -57,7 +57,7 @@ age_groups <- c("85+",
 
 # Folder
 data_folder <- paste0("/conf/quality_indicators/Benchmarking/Cancer QPIs/",
-                      "Data/new_process/testing/test_code_toenails_the_new_process/toenails_july24/")
+                      "Data/new_process/HPB_2024/")
 
 # Folder containing lookup info on HBs by network
 regional_networks_folder <- here("/conf/quality_indicators/Benchmarking/Cancer QPIs/Data/new_process/regional_cancer_networks")
