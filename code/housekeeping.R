7d0dd936a29f14cb8804b1f1b5e319fd8f702c54
CodemakerPHS <pauline.ward5@phs.scot>
2025-10-02 17:27:14 +0000

    added hosps for SCAN for sarcoma

--- code/housekeeping.R
+++ code/housekeeping.R
@@ -37,7 +37,7 @@ meas_vers <- c("3.6", "4.0", "4.0")
 # the names from the most recent published four years of QPIs for this TSG.
 # To use existing names enter a NULL vector e.g. "nca_hosps <- c()"
 nca_hosps <- c("Aberdeen RI", "Woodend Hosp", "Dr Grays Hosp", "Ninewells Hosp", "Raigmore Hosp", "Stracathro Hosp", "Perth Royal Inf")
-sca_hosps <- c("Royal Hosp Edinburgh", "Royal Inf Edinburgh", "Western General Hosp")
+sca_hosps <- c("Borders General Hosp", "Dumfries & Galloway RI", "Victoria Hosp Fife", "QMH Fife", "Royal Inf Edinburgh", "Western General Hosp", "Royal Hosp Sick Children Edinburgh", "St Johns Hosp")
 wos_hosps <- c("Gartnavel (West Glasgow)", "Glasgow Royal Inf", "Queen Elizabeth Hosp")
 
 ## age groups for template :
