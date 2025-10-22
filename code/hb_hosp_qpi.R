2943b551990870d298dbd1c54989ac72a9c9105b
CodemakerPHS <pauline.ward5@phs.scot>
2025-10-22 08:23:46 +0000

    tidying

--- code/hb_hosp_qpi.R
+++ code/hb_hosp_qpi.R
@@ -1,11 +1,9 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # hb_hosp_qpi.R
-# Angus Morton
-# 2023-05-22
 # 
 # Update the hb_hosp_qpi.xlsx file with the new data
 # 
-# R version 4.1.2 (2021-11-01)
+# R version 4.4
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #### Step 0 : Housekeeping ----
