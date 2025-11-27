257feda5ca290c9374521638dfddf25bf2511363
CodemakerPHS <pauline.ward5@phs.scot>
2025-11-27 10:10:53 +0000

    notes about regional data prep

---
+++ code/regional_data_prep.R
@@ -0,0 +1,16 @@
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# 
+# Data prep - regional QPI data for public dashboard
+#
+#
+#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# Lots of manual steps to start with. 
+# See README file: \Cancer_QPIs\Data\new_process\regional_data\nov_2025_dashboard_data
+
+# Folder
+library(here)
+data_folder <- here("/PHI_conf", "CancerGroup2", "Cancer_QPIs",
+                    "Data", "new_process", "regional_data")
+
