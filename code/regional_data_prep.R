2c0c36e56d8ae97546b5dae5432b4751461e0b66
CodemakerPHS <pauline.ward5@phs.scot>
2025-11-28 08:59:02 +0000

    dashboard data conversion outline

--- code/regional_data_prep.R
+++ code/regional_data_prep.R
@@ -7,10 +7,19 @@
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 # Lots of manual steps to start with. 
-# See README file: \Cancer_QPIs\Data\new_process\regional_data\nov_2025_dashboard_data
+# See README file: 
+# \Cancer_QPIs\Data\new_process\regional_data\nov_2025_dashboard_data
+
+library(here) 
+library(tidyverse)
 
 # Folder
-library(here)
 data_folder <- here("/PHI_conf", "CancerGroup2", "Cancer_QPIs",
                     "Data", "new_process", "regional_data")
 
+# Import the three CSV files for performance, age and case ascertainment
+historic_performance <- read_c
+
+# Save as parquet eg
+# #write parquet file to shiny app data folder
+# write_parquet(regional_dataset, "shiny_app/data/regional_dataset", compression = "zstd")
